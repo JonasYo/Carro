13c493c7131d16f838f0331c67955c5f106354ed
TADS <tads1@fapi.local>
2018-04-03 21:42:22 -0300

    Atualizando a montagem do carro

--- fapi_poo_computadorbordo/src/main/java/br/edu/fapi/carro/Carro.java
+++ fapi_poo_computadorbordo/src/main/java/br/edu/fapi/carro/Carro.java
@@ -1,7 +1,12 @@
 package br.edu.fapi.carro;
 
-import br.edu.fapi.tetosolar.TetoSolar;
+import br.edu.fapi.banco.Banco;
+import br.edu.fapi.motor.Motor;
+import br.edu.fapi.pneu.Pneu;
+import br.edu.fapi.roda.Roda;
+import br.edu.fapi.step.Step;
 import br.edu.fapi.tanquegasolina.Tanque;
+import br.edu.fapi.tetosolar.TetoSolar;
 
 public class Carro {
 
@@ -24,5 +29,55 @@ public class Carro {
 	public void setTanqueGasolina(Tanque tanqueGasolina) {
 		this.tanqueGasolina = tanqueGasolina;
 	}
-		
+	
+	private Step step;
+
+	public Step getStep() {
+		return step;
+	}
+
+	public void setStep(Step step) {
+		this.step = step;
+	}
+	
+	private Roda roda;
+
+	public Roda getRoda() {
+		return roda;
+	}
+
+	public void setRoda(Roda roda) {
+		this.roda = roda;
+	}
+	
+	private Pneu pneu;
+
+	public Pneu getPneu() {
+		return pneu;
+	}
+
+	public void setPneu(Pneu pneu) {
+		this.pneu = pneu;
+	}
+	
+	private Motor motor;
+
+	public Motor getMotor() {
+		return motor;
+	}
+
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+	
+	private Banco banco;
+
+	public Banco getBanco() {
+		return banco;
+	}
+
+	public void setBanco(Banco banco) {
+		this.banco = banco;
+	}
+			
 }
--- fapi_poo_computadorbordo/src/main/java/br/edu/fapi/carro/FabricaCarro.java
+++ fapi_poo_computadorbordo/src/main/java/br/edu/fapi/carro/FabricaCarro.java
@@ -2,20 +2,22 @@ package br.edu.fapi.carro;
 
 import java.util.Scanner;
 
+import br.edu.fapi.step.FabricaStep;
+import br.edu.fapi.step.Step;
 import br.edu.fapi.tetosolar.FabricaTetoSolar;
 import br.edu.fapi.tetosolar.TetoSolar;
 
 public class FabricaCarro {
 
 	public Carro fabricarCarro() {
-		System.out.println("Iniciando fabricação do carro :)");
+		System.out.println("Iniciando a fabricação do carro. Por favor responda seguintes perguntas: :)");
 		
 		Scanner scanner = new Scanner(System.in);
 		Carro carro = new Carro();
 		
-		System.out.println("O seu carro possui teto solar ? (sim|nao)");
+		System.out.println("O seu carro irá possuir Teto Solar ? (Sim|Não)");
 		String possuiTetoSolar = scanner.nextLine();
-		if("sim".equalsIgnoreCase(possuiTetoSolar)) {
+		if("Sim".equalsIgnoreCase(possuiTetoSolar)) {
 			FabricaTetoSolar fabricaTetoSolar = new FabricaTetoSolar();
 			double altura = scanner.nextDouble();
 			double largura = scanner.nextDouble();
@@ -25,11 +27,24 @@ public class FabricaCarro {
 			TetoSolar tetoSolar = fabricaTetoSolar.fabricarTetoSolar(altura, largura, espessura, material, cor);
 			
 			carro.setTetoSolar(tetoSolar);
-			
-		
-		
-			
 		}
+		
+		System.out.println("O seu carro irá possuir Step? (Sim|Não)");
+		String possuiStep = scanner.nextLine();
+		if ("Sim".equalsIgnoreCase(possuiStep)) {
+			FabricaStep fabricaStep = new FabricaStep();
+			double aro = scanner.nextDouble();
+			double largura = scanner.nextDouble();
+			double perfil = scanner.nextDouble();
+			Step Step = fabricaStep.fabricaStep(aro, largura, perfil);
+			
+			carro.setStep(Step);
+		}
+		
+		
+		
+			
+		
 		
 		
 		
